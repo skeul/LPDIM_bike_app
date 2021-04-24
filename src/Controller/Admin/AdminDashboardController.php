0c4f830f92d226a32364858b350136b1556725de
skeul <skeul73@live.fr>
2021-04-24 13:51:43 +0200

    Add new dasboard content and add fontawesome

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Velo;
+use App\Repository\UserRepository;
+use App\Repository\ElectricRepository;
+use App\Repository\ParcoursRepository;
+use App\Repository\SommetRepository;
+use App\Repository\SortieRepository;
+use App\Repository\VeloRepository;
+use App\Repository\VTTRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
-    public function index(): Response
-    {
+    public function index(
+        UserRepository $userRepository,
+        ElectricRepository $elecRepository,
+        ParcoursRepository $parcoursRepository,
+        SommetRepository $sommetRepository,
+        SortieRepository $sortieRepository,
+        VTTRepository $vTTRepository,
+        VeloRepository $veloRepository,
+    ): Response {
+
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'AdminDashboardController',
+            'users' => $userRepository->findAll(),
+            'elecs' => $elecRepository->findAll(),
+            'parcours' => $parcoursRepository->findAll(),
+            'sommets' => $sommetRepository->findAll(),
+            'sorties' => $sortieRepository->findAll(),
+            'velo' => $veloRepository->findAll(),
+            'vtts' => $vTTRepository->findAll()
         ]);
     }
 }
