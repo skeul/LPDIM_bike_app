d095d10bc47489ef2a3fbcda923a6a7f74b14da8
skeul <skeul73@live.fr>
2021-04-30 19:23:54 +0200

    Add sortie validators form

--- src/Form/Admin/SortieAdminType.php
+++ src/Form/Admin/SortieAdminType.php
@@ -3,19 +3,49 @@
 namespace App\Form\Admin;
 
 use App\Entity\Sortie;
+use App\Entity\Parcours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class SortieAdminType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('date_sortie')
-            ->add('status')
-            ->add('parcours')
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('date_sortie', DateTimeType::class, [
+                'widget' => 'choice',
+                // prevents rendering it as type="date", to avoid HTML5 date pickers
+                'html5' => false,
+
+            ])
+            ->add('status', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'attr' => ['class' => 'selector'],
+                'choices'  => [
+                    '1' => 'Programée',
+                    '2' => 'Réalisée',
+                ],
+            ])
+            ->add('parcours', EntityType::class, [
+                'class' => Parcours::class,
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'attr' => ['class' => 'selector']
+            ])
             ->add('users');
     }
 
