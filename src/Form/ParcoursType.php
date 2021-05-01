98247f1eeb9f8986f5ad547d25c53eb68252a482
skeul <skeul73@live.fr>
2021-05-01 16:54:33 +0200

    Fix parcours intégration and routes

--- src/Form/ParcoursType.php
+++ src/Form/ParcoursType.php
@@ -2,21 +2,48 @@
 
 namespace App\Form;
 
+use App\Entity\Sommet;
 use App\Entity\Parcours;
+use App\Form\Customs\FloatType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ParcoursType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('distance')
-            ->add('difficulte')
-            ->add('name')
-            ->add('sommet')
-        ;
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('distance', FloatType::class, [
+                'required' => true,
+            ])
+            ->add('difficulte', IntegerType::class, [
+                'required' => true,
+                'help' => '1 Super facile - 10 : Super difficle',
+                'help_attr' => ['class' => 'form-helper'],
+                'attr'  => [
+                    'min' => 1,
+                    'step' => 1,
+                    'max' => 10
+                ],
+            ])
+            ->add('sommet', EntityType::class, [
+                'class' => Sommet::class,
+                'required' => true,
+                'multiple' => true,
+                'expanded' => false,
+                'attr' => ['class' => 'selector-multiple'],
+                'help' => 'Vous pouvez sélectionner plusieurs sommtes pour votre parcours',
+                'help_attr' => ['class' => 'form-helper'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/SommetType.php
+++ src/Form/SommetType.php
@@ -5,18 +5,31 @@ namespace App\Form;
 use App\Entity\Sommet;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SommetType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('altitude')
-            ->add('lieu')
-            ->add('parcours')
-        ;
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('altitude', IntegerType::class, [
+                'required' => true,
+                'attr' => [
+                    'min' => 0,
+                    'step' => 1,
+                    'max' => 4000,
+                ]
+            ])
+            ->add('lieu', TextType::class, [
+                'required' => false,
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver)
