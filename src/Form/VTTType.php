7311c3593fc7d3dbc5e35b894f9865d5ddc34122
Nathan <nathan.charvin@gmail.com>
2021-05-01 22:49:06 +0200

    Update style VTTType form

--- src/Form/VTTType.php
+++ src/Form/VTTType.php
@@ -18,23 +18,28 @@ class VTTType extends AbstractType
     {
         $builder
             ->add('marque', TextType::class, [
+                'attr' => ['class' => 'form-control'],
                 'required' => true,
                 'constraints' => [new Length(['min' => 3])],
             ])
             ->add('modele', TextType::class, [
+                'attr' => ['class' => 'form-control'],
                 'required' => true,
                 'constraints' => [new Length(['min' => 3])],
             ])
             ->add('poids', FloatType::class, [
+                'attr' => ['class' => 'form-control'],
                 'required' => true,
             ])
             ->add('image', UrlType::class, [
+                'attr' => ['class' => 'form-control'],
                 'required' => false,
                 'constraints' => [new Length(['min' => 3])],
             ])
             ->add('suspension_avant', IntegerType::class, [
                 'required' => true,
                 'attr'  => [
+                    'class' => 'form-control',
                     'min' => 50,
                     'step' => 10,
                     'max' => 250
@@ -43,6 +48,7 @@ class VTTType extends AbstractType
             ->add('suspension_arriere', IntegerType::class, [
                 'required' => true,
                 'attr'  => [
+                    'class' => 'form-control',
                     'min' => 50,
                     'step' => 10,
                     'max' => 250
