4115bbadf9c72433399bccfceace7f1cba72a598
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-05-01 23:11:53 +0200

    Fix Kilomètres (status 2 -> 1)

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -59,7 +59,7 @@ class SortieRepository extends ServiceEntityRepository
             ->leftJoin('s.users', 'u', 'WITH', 'u.id = :user')
             ->setParameter('user', $user)
             ->andWhere('u.id IS NOT NULL')
-            ->andWhere('s.status = 2')
+            ->andWhere('s.status = 1')
             ->leftJoin('s.parcours', 'p', 'WITH', 'p.id = s.parcours')
             ->groupBy('u.id')
             ->getQuery()
